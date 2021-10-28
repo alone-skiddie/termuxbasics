38691e4db176add03848498a8624354b462466f9
aloneskid <aloneskiddie@gmail.com>
2021-10-28 11:03:16 +0530

    Add files via upload

---
+++ install-basicpkgs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+sleep 2
+clear && sleep  2
+echo "                                                         " && sleep 2.5
+echo    "             updating and upgrading termux         "  && sleep 0.5 && clear
+echo    "             updating and upgrading termux.         "  && sleep 0.5 && clear
+echo    "             updating and upgrading termux..         "  && sleep 0.5 && clear
+echo    "             updating and upgrading termux...         "  && sleep 2.5 && clear
+echo    "             updating and upgrading termux...         "  && sleep 2.5 
+echo "                                                         "  && sleep 2.5
+sleep 3
+apt update && apt upgrade -y && apt-get update -y && apt-get upgrade -y && pkg update && pkg upgrade -y 
+sleep 3 && clear
+echo "                                                             " && sleep 2.5
+echo  "           installing basic packages for termux            " && sleep 0.5 && clear
+echo  "           installing basic packages for termux.            " && sleep 0.5 && clear
+echo  "           installing basic packages for termux..            " && sleep 0.5 && clear
+echo  "           installing basic packages for termux...            " && sleep 0.5 && clear
+echo  "           installing basic packages for termux...            " && sleep 2.5 
+echo "                                                             " && sleep 2.5
+sleep 3 
+apt install root-repo -y && apt install x11-repo -y && apt install git -y && apt install python -y && apt install python2 -y 
+sleep 1.5 && clear && sleep 1.5 
+apt install ruby -y && apt install wget -y && apt install curl -y && apt install php -y && apt install toilet -y
+sleep 1.5 && clear && sleep 1.5
+apt install perl -y && apt install openssh -y && apt install nmap -y && apt install bash -y && apt install zsh -y 
+sleep 1.5 && clear && sleep 1.5
+apt install php php-apache proot-distro termux-tools -y && apt install apache2 nginx nodejs -y 
+ 
